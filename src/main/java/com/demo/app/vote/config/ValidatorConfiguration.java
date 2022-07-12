3b3a096f9ceb03a015efbb002264add8b517da17
Moises Molina <w7moises@gmail.com>
2022-07-12 14:49:07 -0500

    configuration added

---
+++ src/main/java/com/demo/app/vote/config/ValidatorConfiguration.java
@@ -0,0 +1,27 @@
+package com.demo.app.vote.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.gridfs.ReactiveGridFsTemplate;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ValidatorConfiguration {
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+    @Bean
+    public ReactiveGridFsTemplate reactiveGridFsTemplate(
+            ReactiveMongoDatabaseFactory databaseFactory,
+            MappingMongoConverter mongoConverter) {
+        return new ReactiveGridFsTemplate(databaseFactory, mongoConverter);
+    }
+}
