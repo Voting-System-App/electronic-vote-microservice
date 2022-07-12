fb696cdab2a707821f8818438017118f3740f48c
Moises Molina <w7moises@gmail.com>
2022-07-12 14:44:12 -0500

    template added

---
+++ src/main/java/com/demo/app/vote/ElectronicVoteMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.app.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicVoteMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicVoteMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/app/vote/ElectronicVoteMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.app.vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicVoteMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
