df47d7f79e8e3f0b9d589b51f84d84d2886c068c
Moises Molina <w7moises@gmail.com>
2022-07-14 12:43:07 -0500

    voting date repository added

---
+++ src/main/java/com/demo/app/vote/repositories/VotingDateRepository.java
@@ -0,0 +1,9 @@
+package com.demo.app.vote.repositories;
+
+import com.demo.app.vote.entities.VotingDate;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VotingDateRepository extends ReactiveMongoRepository<VotingDate,String> {
+}
